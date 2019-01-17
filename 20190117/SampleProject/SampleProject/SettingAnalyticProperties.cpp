db89d3657b7d3741fbd79a91f4e9f5565bc7ecf2
tori-takanashi <satokazu.tech@gmail.com>
2019-01-17 20:12:58 +0900

    閉曲面ポリゴンから構造格子を生成し、FDMソルバー(拡散方程式用)で解析した結果をVTKファイルで出力する。

---
+++ 20190117/SampleProject/SampleProject/SettingAnalyticProperties.cpp
@@ -0,0 +1,142 @@
+#include "SettingAnalyticProperties.h"
+#include "VtkIndexUtility.h"
+
+#include <vtkUnstructuredGrid.h>
+#include <vtkDataArray.h>
+#include <vtkIdTypeArray.h>
+#include <vtkCellData.h>
+#include <vtkThreshold.h>
+#include <vtkGeometryFilter.h>
+
+SettingAnalyticProperties::SettingAnalyticProperties()
+{
+}
+
+SettingAnalyticProperties::~SettingAnalyticProperties()
+{
+}
+
+void SettingAnalyticProperties::SetInputData(vtkUnstructuredGrid *in_voxelMesh)
+{
+  this->voxelMeshForAnalysis = vtkSmartPointer<vtkUnstructuredGrid>::New();
+  this->voxelMeshForAnalysis->DeepCopy(in_voxelMesh);
+}
+
+void SettingAnalyticProperties::Update()
+{
+  SetProperties();
+  this->voxelMeshForAnalysis->GetCellData()->AddArray(analyticProperties);
+}
+
+vtkUnstructuredGrid *SettingAnalyticProperties::GetOutput()
+{
+  return voxelMeshForAnalysis;
+}
+
+// 解析場全体の属性を初期化
+void SettingAnalyticProperties::SetProperties()
+{
+  this->analyticProperties = vtkSmartPointer<vtkIdTypeArray>::New();
+  analyticProperties->SetName("AnalyticProoerty");
+  analyticProperties->SetNumberOfComponents(1);
+  analyticProperties->SetNumberOfValues(voxelMeshForAnalysis->GetNumberOfCells());  // 全ボクセル数分だけ配列要素を動的確保
+
+  SetOnePropertyOfAllRegion(Analysis);  // とりあえず、全ボクセルを解析する属性にしておく
+  SetPropertiesOf3DModel();   // 3Dモデル用の解析属性を付与
+  SetPropertiesOfWalls();   // 壁用の解析属性を付与
+}
+
+void SettingAnalyticProperties::SetOnePropertyOfAllRegion(AnalyticProoerty in_prop)
+{
+  for (vtkIdType vId = 0; vId < voxelMeshForAnalysis->GetNumberOfCells(); vId++)
+    this->analyticProperties->SetValue(vId, in_prop);
+}
+
+// 3Dモデルに対して解析用の属性を付与します
+// 表面には境界条件用の属性、内側には非解析領域の属性を与えるとします
+void SettingAnalyticProperties::SetPropertiesOf3DModel()
+{
+  // 何故かvtkDataArrayでないと抽出できない
+  //vtkIdTypeArray *is3DModelIds = vtkIdTypeArray::SafeDownCast(in_grid->GetCellData()->GetArray("Is3DModel"));
+  vtkDataArray *is3DModelIds = voxelMeshForAnalysis->GetCellData()->GetArray("Is3DModel");
+
+  // 3Dモデルの全ボクセルに対して解析属性を付与
+  for (vtkIdType vId = 0; vId < voxelMeshForAnalysis->GetNumberOfCells(); vId++)
+  {
+    //if (is3DModelIds->GetValue(vId) == 1)
+    if (is3DModelIds->GetTuple1(vId) == 1)
+      this->analyticProperties->SetValue(vId, NonAnalysis);
+  }
+
+  auto solid3DModel = vtkSmartPointer<vtkThreshold>::New();
+  solid3DModel->SetInputData(voxelMeshForAnalysis);
+  solid3DModel->SetInputArrayToProcess(0, 0, 0, vtkDataObject::FIELD_ASSOCIATION_CELLS, "Is3DModel");
+  solid3DModel->ThresholdBetween(1, 1);
+  solid3DModel->Update();
+
+  auto surface3DModel = vtkSmartPointer<vtkGeometryFilter>::New();
+  surface3DModel->SetInputData(solid3DModel->GetOutput());
+  surface3DModel->Update();
+
+  auto cellIdList = vtkSmartPointer<vtkIdList>::New();
+  for (vtkIdType vId = 0; vId < surface3DModel->GetOutput()->GetNumberOfCells(); vId++)
+  {
+    vtkIdType oriId = surface3DModel->GetOutput()->GetCellData()->GetArray("OriginalVoxelId")->GetTuple1(vId);
+    cellIdList->InsertUniqueId(oriId);  // 重複なしでIDを入手
+  }
+
+  // 3Dモデル表面のみに解析属性を付与
+  for (vtkIdType vId = 0; vId < cellIdList->GetNumberOfIds(); vId++)
+  {
+    vtkIdType surfId = cellIdList->GetId(vId);
+    this->analyticProperties->SetValue(surfId, Surface3DModel);
+  }
+
+}
+
+void SettingAnalyticProperties::SetPropertiesOfWalls()
+{
+  // X方向の両端にある壁
+  for (vtkIdType zId = 0; zId < cellDims[2]; zId++)
+    for (vtkIdType yId = 0; yId < cellDims[1]; yId++)
+    {
+      vtkIdType minX = 0;
+      vtkIdType vIdMinX = VtkIndexUtility::ConvertXYZToVtkCellId(minX, yId, zId, cellDims);
+
+      vtkIdType maxX = cellDims[0] - 1;
+      vtkIdType vIdMaxX = VtkIndexUtility::ConvertXYZToVtkCellId(maxX, yId, zId, cellDims);
+
+      this->analyticProperties->SetValue(vIdMinX, WallX);
+      this->analyticProperties->SetValue(vIdMaxX, WallX);
+    }
+
+  // Y方向の両端にある壁
+  for (vtkIdType zId = 0; zId < cellDims[2]; zId++)
+    for (vtkIdType xId = 0; xId< cellDims[0]; xId++)
+    {
+      vtkIdType minY = 0;
+      vtkIdType vIdMinY = VtkIndexUtility::ConvertXYZToVtkCellId(xId, minY, zId, cellDims);
+
+      vtkIdType maxY = cellDims[1] - 1;
+      vtkIdType vIdMaxY = VtkIndexUtility::ConvertXYZToVtkCellId(xId, maxY, zId, cellDims);
+
+      this->analyticProperties->SetValue(vIdMinY, WallY);
+      this->analyticProperties->SetValue(vIdMaxY, WallY);
+    }
+
+  // Z方向の両端にある壁
+  for (vtkIdType yId = 0; yId < cellDims[1]; yId++)
+    for (vtkIdType xId = 0; xId < cellDims[0]; xId++)
+    {
+      vtkIdType minZ = 0;
+      vtkIdType vIdMinZ = VtkIndexUtility::ConvertXYZToVtkCellId(xId, yId, minZ, cellDims);
+
+      vtkIdType maxZ = cellDims[2] - 1;
+      vtkIdType vIdMaxZ = VtkIndexUtility::ConvertXYZToVtkCellId(xId, yId, maxZ, cellDims);
+
+      this->analyticProperties->SetValue(vIdMinZ, WallZ);
+      this->analyticProperties->SetValue(vIdMaxZ, WallZ);
+    }
+
+}
+
---
+++ 20190117/SampleProject/SampleProject/main.cpp
@@ -0,0 +1,80 @@
+#include "VoxelMeshGeneratorByVtk.h"
+#include "SettingAnalyticProperties.h"
+#include "FDMSolverForHeatEq.h"
+
+#include <iostream>
+#include <memory>
+
+#include <vtkGeometryFilter.h>
+#include <vtkUnstructuredGrid.h>
+
+// Reader & Writer
+#include <vtkDataSetReader.h>
+#include <vtkSTLReader.h>
+#include <vtkDataSetWriter.h>
+#include <vtkXMLDataSetWriter.h>
+
+using namespace std;
+
+int main()
+{
+  // 閉曲面読み取り
+  //auto reader = vtkSmartPointer<vtkSTLReader>::New();
+  //reader->SetFileName("C:\\sandbox\\stomach_skelly.stl");
+  //reader->SetFileName("C:\\sandbox\\sphere.stl");
+  //reader->SetFileName("C:\\sandbox\\Bayonet.stl");
+  //reader->SetFileName("C:\\sandbox\\sample1.stl");
+
+  auto reader = vtkSmartPointer<vtkDataSetReader>::New();
+  reader->SetFileName("C:\\sandbox\\bunny.vtk");
+  reader->Update();
+
+  // PolyDataに変換
+  auto geometry = vtkSmartPointer<vtkGeometryFilter>::New();
+  geometry->SetInputData(reader->GetOutput());
+  geometry->Update();
+
+  cout << "ボクセルメッシュ作成" << endl;
+  shared_ptr<VoxelMeshGeneratorByVtk> voxelMesh = make_shared<VoxelMeshGeneratorByVtk>();
+  voxelMesh->SetInputData(geometry->GetOutput());
+
+  const int cellDims[] = { 60, 60, 60 };
+  const double *bounds = geometry->GetOutput()->GetBounds();
+  const double offsetX = (bounds[1] - bounds[0]) * 0.2;
+  const double offsetY = (bounds[3] - bounds[2]) * 0.2;
+  const double offsetZ = (bounds[5] - bounds[4]) * 0.2;
+  const double offsets[6] = { offsetX, offsetX, offsetY, offsetY, offsetZ, offsetZ };
+
+  voxelMesh->SetCellDimension(cellDims);
+  voxelMesh->SetOffsets(offsets);
+  voxelMesh->Update();
+
+  cout << "ボクセルメッシュに解析属性を付与" << endl;
+  shared_ptr<SettingAnalyticProperties> voxelMeshWithProperties = make_shared<SettingAnalyticProperties>();
+  voxelMeshWithProperties->SetCellDimension(cellDims);
+  voxelMeshWithProperties->SetInputData(voxelMesh->GetOutput());
+  voxelMeshWithProperties->Update();
+
+  auto field = vtkSmartPointer<vtkUnstructuredGrid>::New();
+  field->DeepCopy(voxelMeshWithProperties->GetOutput());
+
+  cout << "解析開始" << endl;
+  shared_ptr<FDMSolverForHeatEq> solver = make_shared<FDMSolverForHeatEq>();
+
+  const double alpha = 0.1;
+  const double wallXYZBC[3] = { 10.0, 100.0, 40.0 };
+  const int endStep = 200;
+
+  solver->SetAlpha(alpha);
+  solver->SetWallXYZBC(wallXYZBC);
+  solver->SolveHeatEquationForAllStep(field, endStep);
+
+  {
+    auto writer = vtkSmartPointer<vtkXMLDataSetWriter>::New();
+    writer->SetFileName("C:\\sandbox\\Result.vtu");
+    writer->SetInputData(field);
+    writer->Update();
+  }
+
+  return 0;
+}
