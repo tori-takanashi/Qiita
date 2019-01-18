0aa597b418997928c00ae903695e84c0e10a8005
tori-takanashi <satokazu.tech@gmail.com>
2019-01-18 23:00:06 +0900

    フォルダ名変更

---
+++ VTKで作った解析用構造格子を自作FDMソルバーに投げて計算結果をParaViewで見てみる/SampleProject/SampleProject/FDMSolverForDiffusionEq.cpp
@@ -0,0 +1,176 @@
+#include "FDMSolverForDiffusionEq.h"
+#include "vtkSettingAnalyticProperties.h"
+#include "vtkIndexUtility.h"
+
+#include <vtkXMLUnstructuredGridReader.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkDataSetWriter.h>
+#include <vtkXMLDataSetWriter.h>
+#include <vtkDataSet.h>
+#include <vtkDoubleArray.h>
+#include <vtkCellData.h>
+#include <vtkCell.h>
+
+using namespace std;
+
+// 設計適当ですが許して
+// 第一引数：解析場の情報を保存，第二引数：正の値で終了ステップを入力
+void FDMSolverForDiffusionEq::SolveHeatEquationForAllStep(vtkUnstructuredGrid *out_field, const int &in_endStep)
+{
+  vtkIdType NumCellsInfield = out_field->GetNumberOfCells();
+  SetParameters(out_field, alpha); // 解析パラメータ
+  auto initResults = vtkSmartPointer<vtkDoubleArray>::New();
+  initResults->SetName("Results");
+  initResults->SetNumberOfComponents(1);
+  for (auto vId = 0; vId < NumCellsInfield; vId++)
+    initResults->InsertNextTuple1(0.0);
+
+  out_field->GetCellData()->AddArray(initResults);
+
+  // 計算
+  int step;
+  for (step = 0; step < in_endStep; step++)
+  {
+    if (!SolveHeatEquationForOneStep(out_field))
+      break;
+  }
+  cout << step << "で終了" << endl;
+}
+
+// なんかすごいことしてるかもしれないけど許してくれ
+bool FDMSolverForDiffusionEq::SolveHeatEquationForOneStep(vtkUnstructuredGrid *out_field)
+{
+  // 一つ前の結果を取り出す
+  auto beforeResults = vtkSmartPointer<vtkDoubleArray>::New();
+  beforeResults->DeepCopy(vtkDoubleArray::SafeDownCast(out_field->GetCellData()->GetArray("Results")));
+  if (!beforeResults) return false;
+
+  // 解析属性
+  auto properties = out_field->GetCellData()->GetArray("AnalyticProoerty");
+  if (!properties)
+  {
+    cout << "解析用の属性がボクセルに設定されていません。" << endl;
+    return false;
+  }
+
+  // 境界条件
+  SetBoundryCondition(beforeResults, properties);
+
+  // 次の計算結果
+  auto nextResults = vtkSmartPointer<vtkDoubleArray>::New();
+  nextResults->DeepCopy(beforeResults);
+
+  // 全ボクセルを参照
+  for (vtkIdType vId = 0; vId < out_field->GetNumberOfCells(); vId++)
+  {
+    const vtkIdType *xyzId = vtkIndexUtility::ConvertVtkCellIdToXYZ(vId, cellDims);
+
+    // 解析属性がCalculation以外なら飛ばす。
+    if (properties->GetTuple1(vId) != Analysis) continue;
+
+    const double beforeRes = beforeResults->GetTuple1(vId);
+    const vtkIdType *peripheralXYZs = vtkIndexUtility::GetPeripheralXYZs(xyzId[0], xyzId[1], xyzId[2], cellDims);
+    const double deltas[3] = { this->deltas[0], this->deltas[1], this->deltas[2] };
+
+    // 急にラムダ使ってすまない
+    auto getRes = [&](const vtkIdType &cellId) -> const double
+    { return beforeResults->GetTuple1(cellId); };
+
+    // 計算☆
+    const double nextRes = beforeRes + this->dt * this->alpha *
+      (
+        (getRes(peripheralXYZs[0]) - 2.0 * beforeRes + getRes(peripheralXYZs[1])) / (deltas[0] * deltas[0]) +
+        (getRes(peripheralXYZs[2]) - 2.0 * beforeRes + getRes(peripheralXYZs[3])) / (deltas[1] * deltas[1]) +
+        (getRes(peripheralXYZs[4]) - 2.0 * beforeRes + getRes(peripheralXYZs[5])) / (deltas[2] * deltas[2])
+      );
+
+    nextResults->SetTuple1(vId, nextRes);
+  }
+
+  out_field->GetCellData()->AddArray(nextResults);
+  return true;
+}
+
+void FDMSolverForDiffusionEq::SetParameters(vtkUnstructuredGrid *out_field, const double &in_alpha)
+{
+  // out_field の各方向の大きさ
+  double *fieldBounds = out_field->GetBounds();
+  this->lengths[0] = fieldBounds[1] - fieldBounds[0];
+  this->lengths[1] = fieldBounds[3] - fieldBounds[2];
+  this->lengths[2] = fieldBounds[5] - fieldBounds[4];
+
+  // (等間隔格子の場合)どれでもいいのでセルをとってきてその大きさを調べる
+  vtkCell *voxel = out_field->GetCell(0);
+
+  const double voxelSizes[3] =
+  {
+    voxel->GetBounds()[1] - voxel->GetBounds()[0],
+    voxel->GetBounds()[3] - voxel->GetBounds()[2],
+    voxel->GetBounds()[5] - voxel->GetBounds()[4]
+  };
+
+  // XYZ各方向のセル数を調べる
+  this->cellDims[0] = round(this->lengths[0] / voxelSizes[0]);
+  this->cellDims[1] = round(this->lengths[1] / voxelSizes[1]);
+  this->cellDims[2] = round(this->lengths[2] / voxelSizes[2]);
+
+  // 空間刻み幅
+  this->deltas[0] = this->lengths[0] / this->cellDims[0];
+  this->deltas[1] = this->lengths[1] / this->cellDims[1];
+  this->deltas[2] = this->lengths[2] / this->cellDims[2];
+
+  this->alpha = in_alpha;   // > 0
+
+  // フォン・ノイマンの安定性解析に基づき時間刻み幅を設定
+  this->dt = 0.5 / (in_alpha *
+    (
+      1.0 / (this->deltas[0] * this->deltas[0]) +
+      1.0 / (this->deltas[1] * this->deltas[1]) +
+      1.0 / (this->deltas[2] * this->deltas[2])
+    ));
+}
+
+// 周辺6個のボクセル中の値の平均をとる
+void FDMSolverForDiffusionEq::SetAveValueFromSurroundings(const vtkIdType in_voxelId, vtkDoubleArray *out_results, vtkDataArray *in_properties)
+{
+  const vtkIdType *xyzId = vtkIndexUtility::ConvertVtkCellIdToXYZ(in_voxelId, cellDims);
+  const vtkIdType *peripheralXYZs = vtkIndexUtility::GetPeripheralXYZs(xyzId[0], xyzId[1], xyzId[2], cellDims);
+
+  int count = 0;
+  double sumResult = 0.0;
+  for (vtkIdType pxyzId = 0; pxyzId < 6; pxyzId++)
+  {
+    vtkIdType vId = peripheralXYZs[pxyzId];
+
+    // 配列中の確保している要素以外は参照しない
+    if (vId < 0 || out_results->GetNumberOfTuples() <= vId) continue;
+    if (in_properties->GetTuple1(vId) != Analysis) continue;
+
+    sumResult += out_results->GetValue(vId);
+    count++;
+  }
+
+  out_results->SetValue(in_voxelId, sumResult / count);
+}
+
+void FDMSolverForDiffusionEq::SetBoundryCondition(vtkDoubleArray * in_results, vtkDataArray * in_properties)
+{
+  // 全ボクセル参照
+  for (auto vId = 0; vId < in_properties->GetNumberOfTuples(); vId++)
+  {
+    vtkIdType propId = in_properties->GetTuple1(vId);
+
+    // 3Dモデル表面なら、周囲の属性がCalculationのボクセルに定義されている計算値の平均値をセット
+    if (propId == Surface3DModel)
+      SetAveValueFromSurroundings(vId, in_results, in_properties);
+    // 壁
+    else if (propId == WallX)
+      in_results->SetTuple1(vId, wallXYZBC[0]);
+    else if (propId == WallY)
+      in_results->SetTuple1(vId, wallXYZBC[1]);
+    else if (propId == WallZ)
+      in_results->SetTuple1(vId, wallXYZBC[2]);
+  }
+
+}
+
---
+++ VTKで作った解析用構造格子を自作FDMソルバーに投げて計算結果をParaViewで見てみる/SampleProject/SampleProject/vtkIndexUtility.h
@@ -0,0 +1,62 @@
+#pragma once
+
+namespace vtkIndexUtility
+{
+  template<typename T>
+  const T ConvertXYZToVtkCellId(const T & in_x, const T & in_y, const T & in_z, const int in_cellDims[3])
+  {
+    return in_x + in_y * in_cellDims[0] + in_z * in_cellDims[0] * in_cellDims[1];
+  }
+
+  // voxelId -> xyzId
+  // 0 <= xId <= CellDimX - 1, 
+  // 0 <= yId <= CellDimX * CellDimY - 1, 
+  // 0 <= zId <= CellDimX * CellDimY * CellDimZ - 1 ‚Æ‚È‚éB
+  template<typename T>
+  const T * ConvertVtkCellIdToXYZ(const T & in_voxelId, const int in_cellDims[3])
+  {
+    vtkIdType xyzId[3]; // 0:x, 1:y, 2:z
+
+    // zId = vId / (CellDimX * CellDimY)
+    xyzId[2] = in_voxelId / (in_cellDims[0] * in_cellDims[1]);
+
+    // xId + yId * CellDimX = vId % (CellDimX * CellDimY)
+    // yId = vId % (CellDimX * CellDimY) / CellDimX
+    xyzId[1] = (in_voxelId % (in_cellDims[0] * in_cellDims[1])) / in_cellDims[0];
+
+    // xId = (xId + yId * CellDimX) % CellDimX
+    xyzId[0] = (in_voxelId % (in_cellDims[0] * in_cellDims[1]) % in_cellDims[0]);
+
+    return xyzId;
+  }
+
+  // 0:x-1, 1:x+1, 2:y-1, 3:y+1, 4:z-1, 5:z+1
+  template<typename T>
+  const T * GetPeripheralXYZs(const T & in_x, const T & in_y, const T & in_z, const int in_cellDims[3])
+  {
+    const T peripheralXYZs[6] =
+    {
+       ConvertXYZToVtkCellId(in_x - 1, in_y, in_z, in_cellDims),
+       ConvertXYZToVtkCellId(in_x + 1, in_y, in_z, in_cellDims),
+       ConvertXYZToVtkCellId(in_x, in_y - 1, in_z, in_cellDims),
+       ConvertXYZToVtkCellId(in_x, in_y + 1, in_z, in_cellDims),
+       ConvertXYZToVtkCellId(in_x, in_y, in_z - 1, in_cellDims),
+       ConvertXYZToVtkCellId(in_x, in_y, in_z + 1, in_cellDims)
+    };
+    return peripheralXYZs;
+  }
+
+  template<typename T>
+  const bool IsInsideField(const T &in_x, const T &in_y, const T &in_z, const int in_cellDims[3])
+  {
+    const bool isInsideX = (0 < in_x || in_x < in_cellDims[0] - 1);
+    const bool isInsideY = (0 < in_y || in_y < in_cellDims[1] - 1);
+    const bool isInsideZ = (0 < in_z || in_z < in_cellDims[2] - 1);
+
+    if (!isInsideX || !isInsideY || !isInsideZ)
+      return false;
+
+    return true;
+  }
+
+}
